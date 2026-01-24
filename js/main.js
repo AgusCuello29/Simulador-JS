9077a657f2280576fd49b7f9c1de5d2256b7edab
Agustin Cuello <agustincuello231@gmail.com>
2026-01-24 17:34:38 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+const precioBase = 980000;
+const tiposDeConstruccion = ["Económica", "Estándar", "Premium"];
+
+
+function solicitarDatos() {
+    let metros = parseInt(prompt("Ingrese los metros cuadrados a construir:"));
+    
+    while ( metros <= 0) {
+        metros = parseInt(prompt("Ingrese un número válido de metros cuadrados:"));
+    }
+
+    let tipo = prompt(
+        "Seleccione el tipo de construcción:\n" +
+        "1 - Económica\n" +
+        "2 - Estándar\n" +
+        "3 - Premium"
+    );
+
+    return { metros, tipo };
+}
+
+
+function calcularPresupuesto(metros, tipo) {
+    let multiplicador = 1;
+
+    while (tipo != "1" && tipo !== "2" && tipo !== "3") {
+        tipo = prompt ("Seleccione el tipo de construcción:\n" +
+        "1 - Económica\n" +
+        "2 - Estándar\n" +
+        "3 - Premium")
+    };
+
+    if (tipo === "1") {
+        multiplicador = 1;
+    } else if (tipo === "2") {
+        multiplicador = 1.2;
+    } else if (tipo === "3") {
+        multiplicador = 1.4;
+    } 
+
+
+    return metros * precioBase * multiplicador;
+}
+
+
+
+function mostrarResultado(metros, tipo, total) {
+    
+    alert(
+        "Presupuesto estimado:\n" +
+        "Metros cuadrados: " + metros + " m2\n" +
+        "Costo total estimado: $" + total
+    );
+
+    console.log(" PRESUPUESTO ");
+    console.log("Metros cuadrados:", metros);
+    console.log("Costo total:", total);
+}
+
+
+
+function iniciarSimulador() {
+    let continuar = true;
+
+    while (continuar) {
+        let datos = solicitarDatos();
+        let total = calcularPresupuesto(datos.metros, datos.tipo);
+        mostrarResultado(datos.metros, datos.tipo, total);
+
+        continuar = confirm("¿Desea realizar otro cálculo?");
+    }
+
+    alert("Gracias por utilizar el simulador.");
+}
+
+iniciarSimulador();
